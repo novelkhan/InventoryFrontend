c99c5a5a1c840aff2df9237e989e00923e4cbe22
novelkhan <novel4004@gmail.com>
2025-09-21 12:09:15 +0600

    App is fully functional and working properly

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7011',
+  production: true,
+  apiUrl: 'https://inventorywebapi.onrender.com',
   userKey: 'inventoryWebAppUser',
   idleTimeoutInMilliSeconds: 5 * 60 * 1000, // ৫ মিনিট
-  countdownDurationInSeconds: 20 // ২০ সেকেন্ড
+  countdownDurationInSeconds: 30 // ২০ সেকেন্ড
 };

38eebe2bdd1be54448563a70e68c241d6ec3c936
novelkhan <novel4004@gmail.com>
2025-09-23 15:02:16 +0600

    Port is updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://inventorywebapi.onrender.com',
+  // apiUrl: 'https://localhost:7011',  //For development
+  apiUrl: 'https://inventorywebapi.onrender.com',  //For Deployment
   userKey: 'inventoryWebAppUser',
   idleTimeoutInMilliSeconds: 5 * 60 * 1000, // ৫ মিনিট
   countdownDurationInSeconds: 30 // ২০ সেকেন্ড

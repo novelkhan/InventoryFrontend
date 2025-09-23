5ad1eb6edf7bb64407cce6624019488610cf77c8
novelkhan <novel4004@gmail.com>
2025-09-24 02:15:38 +0600

    Register page progressBar added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7011',  //For development
-  // apiUrl: 'https://inventorywebapi.onrender.com',  //For Deployment
+  // apiUrl: 'https://localhost:7011',  //For development
+  apiUrl: 'https://inventorywebapi.onrender.com',  //For Deployment
   userKey: 'inventoryWebAppUser',
   idleTimeoutInMilliSeconds: 5 * 60 * 1000, // ৫ মিনিট
   countdownDurationInSeconds: 30 // ২০ সেকেন্ড
